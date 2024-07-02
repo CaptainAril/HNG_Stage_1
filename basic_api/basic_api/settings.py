8087e8f4c05962d03804b10244bf8c2fc21f244f
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2024-07-02 12:41:45 +0100

    added requirements file

--- basic_api/basic_api/settings.py
+++ basic_api/basic_api/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l3v7y$7*7&4wt=6$rx@yz@7r&ecjf!@yqsa^kg)u^$ydka+%9!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['.localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.localhost', '127.0.0.1', 'eblaze.pythonanywhere.com']
 
 
 # Application definition
